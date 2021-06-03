998ca15e9b00b0a939dcfd6f13552cb04be64ab0
Dax Patel <dax.patel@utoronto.ca>
2021-06-03 18:53:44 -0400

    feat: new mobile friendly design for login/signup pages

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
+import Bubble from './Bubble';
+
 import { connect } from "react-redux";
 import {
   Grid,
@@ -8,11 +10,86 @@ import {
   Button,
   FormControl,
   TextField,
+  Paper,
+  makeStyles,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  paper: {
+    margin: '5% auto auto auto',
+    maxWidth: "900px",
+    [theme.breakpoints.between('xs', 'sm')]: {
+      margin: '2%',
+    },
+  },
+  image: {
+    backgroundImage: `linear-gradient(to bottom, rgba(58, 141, 255, 0.6), rgba(134, 185, 255, 1)), url("/images/bg-img.png")`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    minHeight: "700px",
+    minWidth: "300px",
+    width: "auto",
+    opacity: "85%",
+    [theme.breakpoints.between('xs', 'sm')]: {
+      minHeight: "15px",
+      minWidth: "100%",
+      padding: "5%"
+    },
+
+  },
+  headline: {
+    padding: theme.spacing(3),
+    [theme.breakpoints.between('xs', 'sm')]: {
+      padding: theme.spacing(2),
+    },
+  },
+  form: {
+    padding: theme.spacing(3),
+    margin: "0 5% 0 5%",
+    [theme.breakpoints.between('sm', 'md')]: {
+      margin: "0 15% 0 15%",
+    },
+    [theme.breakpoints.up('lg')]: {
+      margin: "0 12% 0 12%",
+    },
+  },
+  btn: {
+    width: "150px",
+    height: "45px",
+    fontSize: "0.85rem"
+  },
+  btnRegister: {
+    color: "#3A8DFF",
+    backgroundColor: "#FFFFFF",
+  },
+  subText: {
+    fontSize: "0.8rem",
+    color: "#a5a5a5"
+  },
+  welcomeMessage: {
+    fontSize: 22,
+    fontWeight: "bold"
+  },
+  tagLine: {
+    color: "#FFFFFF",
+    fontSize: 22,
+    fontWeight: "light"
+  },
+  bubble: {
+    textAlign: "center"
+  }
+
+}));
+
 const Login = (props) => {
   const history = useHistory();
+  const classes = useStyles();
   const { user, login } = props;
 
   const handleLogin = async (event) => {
@@ -28,41 +105,68 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container>
+          <Grid item container md={5} className={classes.image} justify="center" alignItems="center" alignContent="center">
+            <Grid item xs={12} className={classes.bubble}>
+              <Bubble />
             </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
+            <Grid item>
+              <Typography className={classes.tagLine}>Converse with anyone</Typography>
+            </Grid>
+            <Grid item>
+              <Typography className={classes.tagLine}>with any language</Typography>
             </Grid>
           </Grid>
-        </form>
-      </Box>
-    </Grid>
+          <Grid item md={7} xs={12}>
+            <Grid container item xs={12} justify="flex-end" alignItems="center" className={classes.headline}>
+              <Typography className={classes.subText}>Don't have an account?</Typography>
+              <Box mr={3} />
+              <Button onClick={() => history.push("/register")} variant="contained" size="large" className={`${classes.btnRegister} ${classes.btn}`}>Create account</Button>
+            </Grid>
+            <Grid className={classes.form}>
+              <form onSubmit={handleLogin}>
+                <Grid container spacing={3} >
+                  <Grid item xs={12}>
+                    <Typography className={classes.welcomeMessage}>Welcome back!</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth margin="normal" required>
+                      <TextField
+                        required
+                        aria-label="username"
+                        label="Username"
+                        name="username"
+                        type="text"
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth margin="normal" required>
+                      <TextField
+                        required
+                        label="Password"
+                        aria-label="password"
+                        type="password"
+                        name="password"
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item container xs={12} justify="center">
+                    <Button type="submit" variant="contained" size="large" color="primary" className={classes.btn}>
+                      Login
+                      </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+
+
   );
 };
 
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
+import Bubble from './Bubble';
+
 import {
   Grid,
   Box,
@@ -9,11 +11,85 @@ import {
   FormControl,
   TextField,
   FormHelperText,
+  makeStyles,
+  Paper
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  paper: {
+    margin: '5% auto auto auto',
+    maxWidth: "900px",
+    [theme.breakpoints.between('xs', 'sm')]: {
+      margin: '2%',
+    },
+  },
+  image: {
+    backgroundImage: `linear-gradient(to bottom, rgba(58, 141, 255, 0.6), rgba(134, 185, 255, 1)), url("/images/bg-img.png")`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    minHeight: "700px",
+    minWidth: "300px",
+    width: "auto",
+    opacity: "85%",
+    [theme.breakpoints.between('xs', 'sm')]: {
+      minHeight: "15px",
+      minWidth: "100%",
+      padding: "5%"
+    },
+  },
+  headline: {
+    padding: theme.spacing(3),
+    [theme.breakpoints.between('xs', 'sm')]: {
+      padding: theme.spacing(1),
+    }
+  },
+  form: {
+    padding: theme.spacing(3),
+    margin: "0 5% 0 5%",
+    [theme.breakpoints.between('sm', 'md')]: {
+      margin: "0 15% 0 15%",
+    },
+    [theme.breakpoints.up('lg')]: {
+      margin: "0 12% 0 12%",
+    },
+  },
+  btn: {
+    width: "150px",
+    height: "45px",
+    fontSize: "0.85rem"
+  },
+  btnLogin: {
+    color: "#3A8DFF",
+    backgroundColor: "#FFFFFF",
+  },
+  subText: {
+    fontSize: "0.8rem",
+    color: "#a5a5a5"
+  },
+  welcomeMessage: {
+    fontSize: 22,
+    fontWeight: "bold"
+  },
+  tagLine: {
+    color: "#FFFFFF",
+    fontSize: 22,
+    fontWeight: "light"
+  },
+  bubble: {
+    textAlign: "center"
+  }
+
+}));
+
 const Login = (props) => {
   const history = useHistory();
+  const classes = useStyles();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
@@ -37,73 +113,96 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                  required
-                />
-              </FormControl>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container>
+          <Grid item container md={5} className={classes.image} justify="center" alignItems="center" alignContent="center">
+            <Grid item xs={12} className={classes.bubble}>
+              <Bubble />
             </Grid>
-            <Grid>
-              <FormControl>
-                <TextField
-                  label="E-mail address"
-                  aria-label="e-mail address"
-                  type="email"
-                  name="email"
-                  required
-                />
-              </FormControl>
+            <Grid item>
+              <Typography className={classes.tagLine}>Converse with anyone</Typography>
             </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="password"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
+            <Grid item>
+              <Typography className={classes.tagLine}>with any language</Typography>
             </Grid>
-            <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
-                <TextField
-                  label="Confirm Password"
-                  aria-label="confirm password"
-                  type="password"
-                  inputProps={{ minLength: 6 }}
-                  name="confirmPassword"
-                  required
-                />
-                <FormHelperText>
-                  {formErrorMessage.confirmPassword}
-                </FormHelperText>
-              </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
           </Grid>
-        </form>
-      </Box>
-    </Grid>
+          <Grid item md={7} xs={12}>
+            <Grid container item xs={12} justify="flex-end" alignItems="center" className={classes.headline}>
+              <Typography className={classes.subText}>Already have an account?</Typography>
+              <Box mr={3} />
+              <Button onClick={() => history.push("/login")} variant="contained" size="large" className={`${classes.btnLogin} ${classes.btn}`}>Login</Button>
+            </Grid>
+            <Grid className={classes.form}>
+              <form onSubmit={handleRegister}>
+                <Grid container spacing={3} >
+                  <Grid item xs={12}>
+                    <Typography className={classes.welcomeMessage}>Create an account.</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth margin="normal" required>
+                      <TextField
+                        aria-label="username"
+                        label="Username"
+                        name="username"
+                        type="text"
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth margin="normal" required>
+                      <TextField
+                        label="E-mail address"
+                        aria-label="e-mail address"
+                        type="email"
+                        name="email"
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl error={!!formErrorMessage.confirmPassword} fullWidth margin="normal" required>
+                      <TextField
+                        aria-label="password"
+                        label="Password"
+                        type="password"
+                        inputProps={{ minLength: 6 }}
+                        name="password"
+                        required
+                      />
+                      <FormHelperText>
+                        {formErrorMessage.confirmPassword}
+                      </FormHelperText>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl error={!!formErrorMessage.confirmPassword} fullWidth margin="normal" required>
+                      <TextField
+                        label="Confirm Password"
+                        aria-label="confirm password"
+                        type="password"
+                        inputProps={{ minLength: 6 }}
+                        name="confirmPassword"
+                        required
+                      />
+                      <FormHelperText>
+                        {formErrorMessage.confirmPassword}
+                      </FormHelperText>
+                    </FormControl>
+                  </Grid>
+                  <Grid item container xs={12} justify="center">
+                    <Button type="submit" variant="contained" size="large" color="primary" className={classes.btn}>
+                      Create
+            </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
   );
 };
 

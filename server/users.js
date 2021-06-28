cc086ef1060f9a9b2f073deb63cd6296c38da356
Dax Patel <dax.patel@utoronto.ca>
2021-06-28 15:52:27 -0400

    clean: clean up code and run prettier

--- server/users.js
+++ server/users.js
@@ -1,19 +1,18 @@
 const users = [];
 
 function userJoin(socketId, userId) {
-  const user = {socketId, userId}
+  const user = { socketId, userId };
   users.push(user);
-  console.log(users)
   return user;
 }
 
 function getUserSocketId(userId) {
-  return users.find(user => user.userId === userId);
+  return users.find((user) => user.userId === userId);
 }
 
 function removeUserSocketId(userId) {
-  const user =  users.find(user => user.userId === userId)
-  index = users.indexOf(user);
+  const user = users.find((user) => user.userId === userId);
+  const index = users.indexOf(user);
   if (index > -1) {
     users.splice(index, 1);
   }
